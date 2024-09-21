6a7ed1cbb22d3de61df17ae9b68b1c3658f2b920
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-21 20:00:54 +0100

    Add favoriteBlog + tests

--- src/tests/list_helper.test.ts
+++ src/tests/list_helper.test.ts
@@ -4,23 +4,7 @@ import assert from "node:assert";
 import listHelper from "../utils/list_helper";
 import { Types } from "mongoose";
 
-test("dummy returns one", () => {
-  const blogs: IBlog[] = [];
-  const result = listHelper.dummy(blogs);
-
-  assert.strictEqual(result, 1);
-});
-
-describe("total likes", () => {
-  const listWithOneBlog: IBlog[] = [
-    {
-      id: new Types.ObjectId(),
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-    },
-  ];
+describe("list helper", () => {
   const blogs: IBlog[] = [
     {
       id: new Types.ObjectId(),
@@ -66,18 +50,60 @@ describe("total likes", () => {
     },
   ];
 
-  test("of empty list is zero", () => {
-    const result = listHelper.totalLikes([]);
-    assert.strictEqual(result, 0);
+  const listWithOneBlog: IBlog[] = [
+    {
+      id: new Types.ObjectId(),
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+    },
+  ];
+
+  test("dummy returns one", () => {
+    const blogs: IBlog[] = [];
+    const result = listHelper.dummy(blogs);
+
+    assert.strictEqual(result, 1);
   });
 
-  test("when list has only one blog, equals the likes of that", () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
-    assert.strictEqual(result, 5);
+  describe("total likes", () => {
+    test("of empty list is zero", () => {
+      const result = listHelper.totalLikes([]);
+      assert.strictEqual(result, 0);
+    });
+
+    test("when list has only one blog, equals the likes of that", () => {
+      const result = listHelper.totalLikes(listWithOneBlog);
+      assert.strictEqual(result, listWithOneBlog[0].likes);
+    });
+
+    test("of a bigger list is calculated right", () => {
+      const result = listHelper.totalLikes(blogs);
+      assert.strictEqual(result, 36);
+    });
   });
 
-  test("of a bigger list is calculated right", () => {
-    const result = listHelper.totalLikes(blogs);
-    assert.strictEqual(result, 36);
+  describe("favorite blog", () => {
+    test("of empty list is null", () => {
+      const result = listHelper.favoriteBlog([]);
+      assert.strictEqual(result, null);
+    });
+
+    test("when list has only one blog, equals that blog", () => {
+      const result = listHelper.favoriteBlog(listWithOneBlog);
+
+      const { title, author, likes } = listWithOneBlog[0];
+
+      assert.deepStrictEqual(result, { title, author, likes });
+    });
+
+    test("of a bigger list is the blog with most likes", () => {
+      const result = listHelper.favoriteBlog(blogs);
+
+      const { title, author, likes } = blogs[2];
+
+      assert.deepStrictEqual(result, { title, author, likes });
+    });
   });
 });
--- src/utils/list_helper.ts
+++ src/utils/list_helper.ts
@@ -6,4 +6,14 @@ const totalLikes = (blogs: IBlog[]) => {
   return blogs.reduce((acc, { likes }) => acc + likes, 0);
 };
 
-export default { dummy, totalLikes };
+const favoriteBlog = (blogs: IBlog[]) => {
+  if (blogs.length === 0) return null;
+
+  const { title, author, likes } = blogs.reduce((acc, blog) => {
+    return blog.likes > acc.likes ? blog : acc;
+  });
+
+  return { title, author, likes };
+};
+
+export default { dummy, totalLikes, favoriteBlog };
