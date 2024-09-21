afd3515da9485fac3b9cdbc9e3b1eae718ddc22f
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-09-21 19:00:02 +0100

    Add totalLikes + tests

--- src/tests/list_helper.test.ts
+++ src/tests/list_helper.test.ts
@@ -1,11 +1,83 @@
 import { IBlog } from "../utils/types";
-import { test } from "node:test";
+import { describe, test } from "node:test";
 import assert from "node:assert";
 import listHelper from "../utils/list_helper";
+import { Types } from "mongoose";
 
 test("dummy returns one", () => {
   const blogs: IBlog[] = [];
   const result = listHelper.dummy(blogs);
 
   assert.strictEqual(result, 1);
+});
+
+describe("total likes", () => {
+  const listWithOneBlog: IBlog[] = [
+    {
+      id: new Types.ObjectId(),
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+    },
+  ];
+  const blogs: IBlog[] = [
+    {
+      id: new Types.ObjectId(),
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+    },
+    {
+      id: new Types.ObjectId(),
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+    },
+    {
+      id: new Types.ObjectId(),
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+    },
+    {
+      id: new Types.ObjectId(),
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+    },
+    {
+      id: new Types.ObjectId(),
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+    },
+    {
+      id: new Types.ObjectId(),
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+    },
+  ];
+
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(blogs);
+    assert.strictEqual(result, 36);
+  });
 });
--- src/utils/list_helper.ts
+++ src/utils/list_helper.ts
@@ -2,4 +2,8 @@ import { IBlog } from "./types";
 
 const dummy = (_blogs: IBlog[]) => 1;
 
-export default { dummy };
+const totalLikes = (blogs: IBlog[]) => {
+  return blogs.reduce((acc, { likes }) => acc + likes, 0);
+};
+
+export default { dummy, totalLikes };
